cff174f3e10bf668c5a2caafb3cc5fdea5ac9531
jacobcarvell <jacobcarvell@gmail.com>
2016-09-02 23:24:36 +1000

    added styling to the top bar

--- app/components/TopBar.jsx
+++ app/components/TopBar.jsx
@@ -7,13 +7,14 @@ var TopBar = React.createClass({
     return (
       <div className="page-top-bar">
         <div className="topbar-title">
-
           <h3>{pageName}</h3>
         </div>
-        <span className="info badge"><i className="fa fa-check"></i></span>
-        <div className="small button-group">
-          <a className="button">Trace</a>
-          <a className="button">Data</a>
+        <div className="flex-right">
+          <i className="fa fa-check-circle fa-2x completed" id="toggle-completed"></i>
+          <div className="small button-group">
+            <a className="button active">Trace</a>
+            <a className="button">Data</a>
+          </div>
         </div>
       </div>
     )
